90fabf1bb09a2848aa04e0f72467f948f28ae6a8
Rocco Howard <rocco@hnh.digital>
2017-06-20 12:54:27 +0000

    Moved configuration to the `Slack` class

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace Bluora\LaravelSlack;
 
-use GuzzleHttp\Client as Guzzle;
-
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     /**
@@ -42,23 +40,10 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'slack');
 
         $this->app->singleton('bluora.slack', function ($app) {
-            return new Slack(
-                $app['config']->get('slack.endpoint'),
-                [
-                    'channel'                 => $app['config']->get('slack.channel'),
-                    'username'                => $app['config']->get('slack.username'),
-                    'icon'                    => $app['config']->get('slack.icon'),
-                    'link_names'              => $app['config']->get('slack.link_names'),
-                    'unfurl_links'            => $app['config']->get('slack.unfurl_links'),
-                    'unfurl_media'            => $app['config']->get('slack.unfurl_media'),
-                    'allow_markdown'          => $app['config']->get('slack.allow_markdown'),
-                    'markdown_in_attachments' => $app['config']->get('slack.markdown_in_attachments'),
-                ],
-                new Guzzle()
-            );
+            return new Slack($app);
         });
 
-        $this->app->bind('Bluora\Slack\Client', 'bluora.slack');
+        $this->app->bind('Bluora\LaravelSlack\Slack', 'bluora.slack');
     }
 
     /**
--- src/Slack.php
+++ src/Slack.php
@@ -7,9 +7,31 @@ use Bluora\Slack\Attachment;
 use Bluora\Slack\AttachmentAction;
 use Bluora\Slack\AttachmentField;
 use Bluora\Slack\Client;
+use GuzzleHttp\Client as Guzzle;
 
 class Slack extends Client
 {
+
+    public function __construct($app)
+    {
+        parent::__construct(
+            $app['config']->get('slack.endpoint'),
+            [
+                'channel'                 => $app['config']->get('slack.channel'),
+                'username'                => $app['config']->get('slack.username'),
+                'icon'                    => $app['config']->get('slack.icon'),
+                'link_names'              => $app['config']->get('slack.link_names'),
+                'unfurl_links'            => $app['config']->get('slack.unfurl_links'),
+                'unfurl_media'            => $app['config']->get('slack.unfurl_media'),
+                'allow_markdown'          => $app['config']->get('slack.allow_markdown'),
+                'markdown_in_attachments' => $app['config']->get('slack.markdown_in_attachments'),
+            ],
+            new Guzzle()
+        );
+
+        return $this;
+    }
+
     /**
      * Create a new attahcment.
      *
